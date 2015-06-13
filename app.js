5163f533ff17b8a04cccc05e3eb36dcef66b77a6
Alexey <alexey.ernest@gmail.com>
2015-06-14 02:54:25 +0300

    Init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.get('/', function (req, res) {
+    res.send('hello app\n');
+});
+
+module.exports = app;

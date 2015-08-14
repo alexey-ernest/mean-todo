39fc70b00308d9a5171a72cf7ef34d65778c07aa
Alexey Melnikov <alexey.ernest@gmail.com>
2015-08-14 15:18:28 +0300

    Fix env.sh script

--- env.sh
+++ env.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-# Docker doesn't have a great way to set runtime environment variables,
-# so use this script to prepare the execution environnment for later processes.
-# Original: http://mike-clarke.com/2013/11/docker-links-and-runtime-env-vars/
+#!/bin/sh
 export MONGO_CONNECTION="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/todo"
-
-# Execute the commands passed to this script
-# e.g. "./env.sh venv/bin/nosetests --with-xunit
-exec "$@"

f83e438f4ff7572e029b80b9427cc13105cd5a4c
Alexey <alexey.ernest@gmail.com>
2015-08-14 20:26:16 +0300

    Entrypoint script fix

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# set env variables
+export MONGO_CONNECTION="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/todo"
+
+# execute nodejs application
+exec /nodejs/bin/npm start
+
+echo 'OK!'

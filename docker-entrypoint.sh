1b50cb021f837cc783cbb85777e711a97578b44a
Alexey <alexey.ernest@gmail.com>
2015-08-15 15:01:59 +0300

    Set MONGO_CONNECTION only if mongo container linked

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 # set env variables
-export MONGO_CONNECTION="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/todo"
+if [ -n "$MONGO_PORT_27017_TCP_ADDR" ] && [ -n "$MONGO_PORT_27017_TCP_PORT" ]; then
+  export MONGO_CONNECTION="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/todo"
+fi
+
+echo "USING MONGO CONNECTION: ${MONGO_CONNECTION}"
 
 # execute nodejs application
 exec /nodejs/bin/npm start
